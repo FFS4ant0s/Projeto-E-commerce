c44fd98d7a53aae7fce0e189d02dd24c4b21bb1d
Fernando <silentguyband@gmail.com>
2024-10-21 11:40:04 -0300

    Created templates

--- produto/views.py
+++ produto/views.py
@@ -2,11 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
+from . import models
 
 
 class ListaProdutos(ListView):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Lista produtos')
+    model = models.Produto
+    template_name = 'produto/lista.html'
+    context_object_name = 'produtos'
+    paginate_by = 10
 
 
 class DetalheProdutos(View):
