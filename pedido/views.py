f9612231c9b2e5547cd247a947ccee22bfa97290
Fernando <silentguyband@gmail.com>
2024-11-18 11:06:17 -0300

    Save order part 1/4

--- pedido/views.py
+++ pedido/views.py
@@ -1,11 +1,65 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import ListView
+from django.contrib import messages
+
+from produto.models import Variacao
 
 
 class Pagar(View):
+    template_name = 'pedido/pagar.html'
+
     def get(self, *args, **kwargs):
-        return HttpResponse('Pagar')
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                'Você precisa fazer login.'
+            )
+
+            return redirect('perfil:criar')
+
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Carrinho vazio.'
+            )
+
+            return redirect('produto:lista')
+
+        carrinho = self.request.session.get('carrinho')
+        carrinho_variacao_ids = [v for v in carrinho]
+        bd_variacoes = list(Variacao.objects.select_related('produto').filter(id__in=carrinho_variacao_ids))
+
+        for variacao in bd_variacoes:
+            vid = str(variacao.id)
+            estoque = variacao.estoque
+            qtd_carrinho = carrinho[vid]['quantidade']
+            preco_unt = carrinho[vid]['preco_unitario']
+            preco_unt_promo = carrinho[vid]['preco_unitario_promocional']
+            error_msg_estoque = ''
+
+            if estoque < qtd_carrinho:
+                carrinho[vid]['quantidade'] = estoque
+                carrinho[vid]['preco_quantitativo'] = estoque * preco_unt
+                carrinho[vid]['preco_quantitativo_promocional'] = estoque * preco_unt_promo
+
+                error_msg_estoque = 'Estoque insuficiente para alguns produtos do seu carrinho. '\
+                    'Reduzimos a quantidade desses produtos. Por favor, verifique '\
+                    'nos quais produtos foram afetados a seguir.'
+
+            if error_msg_estoque:
+                messages.error(
+                    self.request,
+                    error_msg_estoque
+                )
+
+                self.request.session.save()
+                return redirect('produto:carrinho')
+
+        contexto = {}
+
+        return render(self.request, self.template_name, contexto)
 
 
 class SalvarPedido(View):
